2d78264387a8c64f07a5284e2cad2dc39e80710d
Cédric Félizard <cedric@felizard.fr>
2012-09-09 13:55:04 +0200

    Produce coverage report

--- test.sh
+++ test.sh
@@ -7,6 +7,9 @@ for version in 2 3; do
     [ -d envs/$version ] || virtualenv -p python$version envs/$version
     . envs/$version/bin/activate
     pip install -r requirements.txt --upgrade --use-mirrors
-    nosetests
+    case $version in
+        2) nosetests ;;
+        3) nosetests --with-coverage --cover-html --cover-package=zeyple ;;
+    esac
 done
 
