4356edc709d159521da24cbc71d215088850cda8
Cédric Félizard <cedric@felizard.fr>
2014-09-14 14:59:01 +0000

    Tweak the test script

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 [ -d envs ] || mkdir envs
 for version in 3 2; do
     [ -d envs/$version ] || virtualenv -p python$version envs/$version
     . envs/$version/bin/activate
-    pip install -r requirements.txt --upgrade --use-mirrors
+    pip install -r requirements.txt --upgrade
     case $version in
         2) nosetests ;;
         3) nosetests --with-coverage --cover-html --cover-package=zeyple ;;
