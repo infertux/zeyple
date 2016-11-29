47df3110999dbde677a0e1bde41f0ded8b6113e0
Cédric Félizard <cedric@felizard.fr>
2016-11-29 18:29:22 +0000

    Add simple upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+ZEYPLE_PATH=/usr/sbin/zeyple
+
+cd $(dirname $0)
+
+wget https://raw.github.com/infertux/zeyple/master/zeyple/zeyple.py -O zeyple.py
+sudo mv zeyple.py $ZEYPLE_PATH
+sudo chmod 500 $ZEYPLE_PATH
+sudo chown zeyple: $ZEYPLE_PATH
+sudo chcon unconfined_u:object_r:bin_t:s0 $ZEYPLE_PATH || true # if you use SELinux
