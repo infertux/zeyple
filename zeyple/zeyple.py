0dee7b46ab770a142c2af006299d03d38b20a795
Harald Wilhelmi <harald.wilhelmi@tngtech.com>
2019-10-25 10:53:23 +0200

    New feature: Missing keys - rules and notifications

--- zeyple/zeyple.py
+++ zeyple/zeyple.py
@@ -7,10 +7,39 @@ import logging
 import email
 import email.mime.multipart
 import email.mime.application
+from email.mime.text import MIMEText
 import email.encoders
 import smtplib
 import copy
 from io import BytesIO
+import re
+
+
+MISSING_KEY_RULES_SECTION = 'missing_key_rules'
+
+ACTION_DROP = 'drop'
+ACTION_NOTIFY = 'notify'
+ACTION_CLEARTEXT = 'cleartext'
+
+VALID_ACTIONS = [
+  ACTION_DROP,
+  ACTION_NOTIFY,
+  ACTION_CLEARTEXT,
+]
+
+DEFAULT_MISSING_KEY_NOTIFICATION = """
+Hello,
+
+the sender of this message tried to encrypt a message for you
+with PGP using a Zeyple encryption gateway. Unfortunately that
+failed. Zeyple was configured to drop the message for security
+reasons. If you supply your PGP public key to the sender, you
+may have better luck next time.
+
+Sorry
+Your Zeyple
+"""
+
 
 try:
     from configparser import SafeConfigParser  # Python 3
@@ -51,7 +80,7 @@ def encode_string(string):
 
 
 __title__ = 'Zeyple'
-__version__ = '1.2.2'
+__version__ = '1.2.2-tng2'
 __author__ = 'Cédric Félizard'
 __license__ = 'AGPLv3+'
 __copyright__ = 'Copyright 2012-2018 Cédric Félizard'
@@ -62,12 +91,12 @@ class Zeyple:
 
     def __init__(self, config_fname='zeyple.conf'):
         self.config = self.load_configuration(config_fname)
-
         log_file = self.config.get('zeyple', 'log_file')
         logging.basicConfig(
             filename=log_file, level=logging.DEBUG,
             format='%(asctime)s %(process)s %(levelname)s %(message)s'
         )
+        self.missing_key_oracle = _MissingKeyOracle(self.config)
         logging.info("Zeyple ready to encrypt outgoing emails")
 
     def load_configuration(self, filename):
@@ -80,6 +109,12 @@ class Zeyple:
         ])
         if not config.sections():
             raise IOError('Cannot open config file.')
+
+        if config.has_option('zeyple', 'missing_key_notification_body'):
+            with open(config.get('zeyple', 'missing_key_notification_body')) as handle:
+                self.missing_key_notification = handle.read()
+        else:
+            self.missing_key_notification = DEFAULT_MISSING_KEY_NOTIFICATION
         return config
 
     @property
@@ -121,26 +156,41 @@ class Zeyple:
         for recipient in recipients:
             logging.info("Recipient: %s", recipient)
 
-            key_id = self._user_key(recipient)
-            logging.info("Key ID: %s", key_id)
-
-            if key_id:
-                out_message = self._encrypt_message(in_message, key_id)
-
-            elif self.config.has_option('zeyple', 'force_encrypt') and \
-                    self.config.getboolean('zeyple', 'force_encrypt'):
-                logging.error("No keys found, message will not be sent!")
+            out_message = self._get_message(in_message, recipient)
+            if out_message is None:
                 continue
-
-            else:
-                logging.warn("No keys found, message will be sent unencrypted")
-                out_message = copy.copy(in_message)
 
             self._add_zeyple_header(out_message)
             self._send_message(out_message, recipient)
             sent_messages.append(out_message)
 
         return sent_messages
+
+    def _get_message(self, in_message, recipient):
+        key_id = self._user_key(recipient)
+        logging.info("Key ID: %s", key_id)
+        if key_id:
+            return self._encrypt_message(in_message, key_id)
+        action = self.missing_key_oracle.get_action(recipient)
+        if action == ACTION_DROP:
+            logging.error("No keys found, message will not be sent!")
+            return None
+        elif action == ACTION_CLEARTEXT:
+            logging.warn("No keys found, message will be sent unencrypted")
+            return copy.copy(in_message)
+        else:
+            logging.warn("No keys found, sending notification to recipient")
+            return self._get_missing_key_message(in_message, recipient)
+
+    def _get_missing_key_message(self, in_message, recipient):
+        out_message = MIMEText(self.missing_key_notification)
+        if self.config.has_option('zeyple', 'missing_key_notification_subject'):
+            out_message['Subject'] = self.config.get('zeyple', 'missing_key_notification_subject')
+        else:
+            out_message['Subject'] = 'Missing PGP key'
+        out_message['To'] = recipient
+        out_message['From'] = in_message['From']
+        return out_message
 
     def _get_version_part(self):
         ret = email.mime.application.MIMEApplication(
@@ -304,13 +354,62 @@ class Zeyple:
         """Sends the given message through the SMTP relay"""
         logging.info("Sending message %s", message['Message-id'])
 
-        smtp = smtplib.SMTP(self.config.get('relay', 'host'),
-                            self.config.getint('relay', 'port'))
+        smtp = smtplib.SMTP(
+            self.config.get('relay', 'host'),
+            self.config.getint('relay', 'port')
+        )
 
         smtp.sendmail(message['From'], recipient, message.as_string())
         smtp.quit()
 
         logging.info("Message %s sent", message['Message-id'])
+
+
+class _ActionRule:
+    def __init__(self, pattern, action):
+        if action not in VALID_ACTIONS:
+            logging.error(
+                "Pattern '{0}' has bad action! Must be one of: {1}".format(
+                    pattern, ', '.join(VALID_ACTIONS)
+                )
+            )
+        self.pattern = re.compile(pattern)
+        self.action = action
+
+    def check(self, email):
+        if self.pattern.match(email):
+            return self.action
+        else:
+            return None
+
+
+class _MissingKeyOracle:
+    def __init__(self, config=None):
+        self._rules = []
+        if config is not None:
+            self.load_configuration(config)
+
+    def load_configuration(self, config):
+        if config.has_section(MISSING_KEY_RULES_SECTION):
+            for option in config.options(MISSING_KEY_RULES_SECTION):
+                value = config.get(MISSING_KEY_RULES_SECTION, option)
+                self._rules.append(_ActionRule(option, value))
+
+        if config.has_option('zeyple', 'force_encrypt'):
+            if config.getboolean('zeyple', 'force_encrypt'):
+                action = ACTION_DROP
+            else:
+                action = ACTION_CLEARTEXT
+            logging.warn('Found deprecated configuration parameter force_encrypt!')
+            logging.warn('Please use a [{0}] section instead.'.format(MISSING_KEY_RULES_SECTION))
+            logging.warn("The entry '. = {0}' will do what you want.".format(action))
+
+    def get_action(self, email):
+        for rule in self._rules:
+            action = rule.check(email)
+            if action is not None:
+                return action
+        return ACTION_NOTIFY
 
 
 if __name__ == '__main__':
